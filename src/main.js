50b4b0ea463ae75a0087fb9c22db598e05b476ae
Otavio <otavio.zerotwo@gmail.com>
2025-08-31 15:30:31 -0300

    feat: add faq section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const buttons = document.querySelectorAll('[data-tab-button]');
+  const questions = document.querySelectorAll('[data-faq-question]');
   
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(button) {
@@ -13,7 +14,18 @@ document.addEventListener('DOMContentLoaded', function() {
       button.target.classList.add('shows__tabs__button--is-active');
     })
   }
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', toggleAnswer);
+  }
 });
+
+function toggleAnswer(element) {
+  const classClicked = 'faq__questions__item--is-open';
+  const parentElement = element.target.parentNode;
+
+  parentElement.classList.toggle(classClicked);
+}
 
 function removeClassActiveButton() {
   const buttons = document.querySelectorAll('[data-tab-button]');
